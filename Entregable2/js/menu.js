13fbc2e2a8adff78b603f8c4b70acebbecda00a3
Valentino <malassisivalen@gmail.com>
2024-09-26 00:42:12 -0300

    cambios

---
+++ Entregable2/js/menu.js
@@ -0,0 +1,10 @@
+let menu=document.querySelector(".menu");
+menu.addEventListener('click',show_menu);
+
+let categorias=document.querySelector(".categorias");
+
+
+function show_menu(){
+    categorias.style.display = "flex";
+} 
+
